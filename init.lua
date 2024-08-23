1194914e1a594645f1d65db88fcd36677c8863cf
Athavan Kanapuli <athavankanapuli@gmail.com>
2024-08-23 10:53:56 +0200

    Add neotree

--- init.lua
+++ init.lua
@@ -320,6 +320,17 @@ require('lazy').setup({
     end,
   },
 
+  -- Neo-tree
+  {
+    'nvim-neo-tree/neo-tree.nvim',
+    branch = 'v3.x',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+      'MunifTanjim/nui.nvim',
+      '3rd/image.nvim', -- Optional image support in preview window: See `# Preview Mode` for more information
+    },
+  },
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
   -- keys can be used to configure plugin behavior/loading/etc.
