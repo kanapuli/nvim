800d6678f3f2bcc05d892a9a45289072002a0681
Athavan Kanapuli <athavankanapuli@gmail.com>
2024-09-06 10:51:54 +0200

    Add trouble nvim

--- init.lua
+++ init.lua
@@ -268,6 +268,12 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+-- run jsonlint for json files
+vim.api.nvim_create_autocmd('BufWritePost', {
+  pattern = '*.json',
+  command = '!jsonlint % -q',
+})
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
