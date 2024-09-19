9b8b62d358992c2a65449ea54f63f0c6363f717d
Athavan Kanapuli <athavankanapuli@gmail.com>
2024-09-19 10:02:48 +0000

    Fix keymaps for easymotion

--- init.lua
+++ init.lua
@@ -336,12 +336,10 @@ require('lazy').setup({
   {
     'easymotion/vim-easymotion',
     config = function()
-      vim.keymap.set('n', '<localleader>f', '<Plug>(easymotion-overwin-f)', { desc = '[S]earch [O]ne [L]etter' })
-      vim.keymap.set('n', '<localleader>s', '<Plug>(easymotion-overwin-f2)', { desc = '[S]earch [T]wo [L]etter' })
-      -- vim.keymap.set('n', 's', '<Plug>(easymotion-s2)', { desc = '[S]earch [T]wo [C]har' })
+      vim.keymap.set('n', 'f', '<Plug>(easymotion-overwin-f)', { desc = '[S]earch [O]ne [L]etter' })
+      vim.keymap.set('n', 's', '<Plug>(easymotion-s2)', { desc = '[S]earch [T]wo [C]har' })
       vim.keymap.set('n', 't', '<Plug>(easymotion-t2)', { desc = '[S]earch [T]wo [C]har' })
       vim.keymap.set('n', '/', '<Plug>(easymotion-sn)', { desc = '[S]earch [N] [C]har' })
-      vim.keymap.set('n', 's', '<Plug>(easymotion-s)', { desc = '[S]earch [R]epeat [C]har' })
       vim.keymap.set('n', '<localleader>l', '<Plug>(easymotion-overwin-line)', { desc = '[M]ove to [L]ine' })
       vim.keymap.set('n', '<localleader>w', '<Plug>(easymotion-overwin-w)', { desc = '[M]ove to [W]ord' })
       vim.keymap.set('n', 'n', '<Plug>(easymotion-next)', { desc = '[M]ove to [N]ext' })
