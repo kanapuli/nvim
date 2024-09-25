f7060b2f0fdef3548c4ff32f0c41036f680d86db
Athavan Kanapuli <athavankanapuli@gmail.com>
2024-09-25 06:24:46 +0200

    Add matchup plugin

--- lua/custom/plugins/lsp_signature.lua
+++ lua/custom/plugins/lsp_signature.lua
@@ -2,7 +2,12 @@ return {
   'ray-x/lsp_signature.nvim',
   event = 'VeryLazy',
   opts = {},
-  config = function(_, opts)
-    require('lsp_signature').on_attach(opts)
+  config = function(_, _)
+    require('lsp_signature').setup {
+      doc_lines = 0,
+      handler_opts = {
+        border = none,
+      },
+    }
   end,
 }
---
+++ lua/custom/plugins/matchup.lua
@@ -0,0 +1,4 @@
+return {
+  -- better %
+  'andymass/vim-matchup',
+}
