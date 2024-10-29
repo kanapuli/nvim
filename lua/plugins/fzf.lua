328a99782a369ca9662f6fdfa13f726e290e74f4
Athavan Kanapuli <athavankanapuli@gmail.com>
2024-10-29 05:29:20 +0000

    Add fzf lua

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -1,28 +1,50 @@
+-- return {
+--   'junegunn/fzf.vim',
+--   dependencies = {
+--     'junegunn/fzf',
+--   },
+--   build = function()
+--     vim.fn['fzf#install']()
+--   end,
+--   config = function()
+--     -- stop putting a giant window over my editor
+--     vim.g.fzf_layout = { down = '~20%' }
+--     -- when using :Files, pass the file list through
+--     --
+--     --   https://github.com/jonhoo/proximity-sort
+--     --
+--     -- to prefer files closer to the current file.
+--     function list_cmd()
+--       local base = vim.fn.fnamemodify(vim.fn.expand '%', ':h:.:S')
+--       if base == '.' then
+--         -- if there is no current file,
+--         -- proximity-sort can't do its thing
+--         return 'fd --type file --follow'
+--       else
+--         return vim.fn.printf('fd --type file --follow | proximity-sort %s', vim.fn.shellescape(vim.fn.expand '%'))
+--       end
+--     end
+--     vim.api.nvim_create_user_command('Files', function(arg)
+--       vim.fn['fzf#vim#files'](arg.qargs, { source = list_cmd(), options = '--tiebreak=index' }, arg.bang)
+--     end, { bang = true, nargs = '?', complete = 'dir' })
+--   end,
+-- }
+
 return {
-  'junegunn/fzf.vim',
+  'ibhagwan/fzf-lua',
+  -- optional for icon support
   dependencies = {
-    { 'junegunn/fzf', dir = '~/.fzf', build = './install --all' },
+    'nvim-tree/nvim-web-devicons',
+    { 'junegunn/fzf', run = './install --bin' },
   },
   config = function()
-    -- stop putting a giant window over my editor
-    vim.g.fzf_layout = { down = '~20%' }
-    -- when using :Files, pass the file list through
-    --
-    --   https://github.com/jonhoo/proximity-sort
-    --
-    -- to prefer files closer to the current file.
-    function list_cmd()
-      local base = vim.fn.fnamemodify(vim.fn.expand '%', ':h:.:S')
-      if base == '.' then
-        -- if there is no current file,
-        -- proximity-sort can't do its thing
-        return 'fd --type file --follow'
-      else
-        return vim.fn.printf('fd --type file --follow | proximity-sort %s', vim.fn.shellescape(vim.fn.expand '%'))
-      end
-    end
-    vim.api.nvim_create_user_command('Files', function(arg)
-      vim.fn['fzf#vim#files'](arg.qargs, { source = list_cmd(), options = '--tiebreak=index' }, arg.bang)
-    end, { bang = true, nargs = '?', complete = 'dir' })
+    -- calling `setup` is optional for customization
+    require('fzf-lua').setup { 'fzf-native' }
+    local f = require 'fzf-lua'
+    vim.keymap.set('n', '<c-p>', "<cmd>lua require('fzf-lua').files()<CR>", { noremap = true, silent = true })
+    vim.keymap.set('n', '<c-e>', f.oldfiles, { noremap = true, silent = true })
+    vim.keymap.set('n', '<leader>sg', f.live_grep, { noremap = true, silent = true })
+    vim.keymap.set('n', '<leader>sb', f.buffers, { noremap = true, silent = true })
+    vim.keymap.set('n', '<leader>sh', f.builtin, { noremap = true, silent = true })
   end,
 }
