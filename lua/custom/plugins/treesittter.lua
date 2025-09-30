ff7360bdf6fc111996287706783db4e2d5b0965f
Athavan Kanapuli <athavankanapuli@gmail.com>
2025-09-30 17:05:18 +0200

    add more treesitter parser

--- lua/custom/plugins/treesittter.lua
+++ lua/custom/plugins/treesittter.lua
@@ -7,6 +7,30 @@ return {
     require('nvim-treesitter').setup {
       install_dir = vim.fn.stdpath 'data' .. '/site',
     }
-    require('nvim-treesitter').install { 'rust', 'javascript', 'zig', 'go', 'gomod', 'gowork', 'gotmpl', 'gosum', 'python', 'markdown' }
+    require('nvim-treesitter').install {
+      'rust',
+      'javascript',
+      'zig',
+      'go',
+      'gomod',
+      'gowork',
+      'gotmpl',
+      'gosum',
+      'python',
+      'markdown',
+      'rego',
+      'c',
+      'clojure',
+      'gitcommit',
+      'gitignore',
+      'json',
+      'kotlin',
+      'lua',
+      'luadoc',
+      'yaml',
+      'toml',
+      'tmux',
+      'typescript',
+    }
   end,
 }
