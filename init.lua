31c381d5554b81d1cd98085dd16fa6593a871546
Athavan Kanapuli <athavankanapuli@gmail.com>
2025-09-30 22:09:58 +0200

    add python tools

--- init.lua
+++ init.lua
@@ -724,8 +724,16 @@ require('lazy').setup({
             },
           },
         },
-        -- pyright = {},
-        -- rust_analyzer = {},
+        ruff = {
+          cmd_env = { RUFF_TRACE = 'messages' },
+          init_options = {
+            settings = {
+              logLevel = 'error',
+            },
+          },
+        },
+        pyright = {},
+        rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
@@ -774,6 +782,8 @@ require('lazy').setup({
         'delve',
         'golangci-lint',
         'golangci-lint-langserver',
+        'ruff',
+        'pyright',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
---
+++ lua/custom/plugins/python-tools.lua
@@ -0,0 +1,24 @@
+return {
+  'nvim-neotest/neotest-python',
+  {
+    'linux-cultist/venv-selector.nvim',
+    cmd = 'VenvSelect',
+    opts = {
+      options = {
+        notify_user_on_venv_activation = true,
+      },
+    },
+    --  Call config for Python files and load the cached venv automatically
+    ft = 'python',
+    keys = { { '<leader>cv', '<cmd>:VenvSelect<cr>', desc = 'Select VirtualEnv', ft = 'python' } },
+  },
+  {
+    'mfussenegger/nvim-dap-python',
+  -- stylua: ignore
+  keys = {
+    { "<leader>dPt", function() require('dap-python').test_method() end, desc = "Debug Method", ft = "python" },
+    { "<leader>dPc", function() require('dap-python').test_class() end, desc = "Debug Class", ft = "python" },
+  }
+,
+  },
+}
