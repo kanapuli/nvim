1d54843e70a8a134ee6930a4b67abec553a7f938
Athavan Kanapuli <athavankanapuli@gmail.com>
2025-09-30 16:10:17 +0200

    Add neotest

--- init.lua
+++ init.lua
@@ -982,7 +982,8 @@ require('lazy').setup({
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
-    main = 'nvim-treesitter.configs', -- Sets main module to use for opts
+    branch = 'main',
+    -- main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
       ensure_installed = {
