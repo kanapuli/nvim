600e705791117f1e569aff8a30f8a47a83f87d8d
Athavan Kanapuli <athavankanapuli@gmail.com>
2025-10-01 06:39:36 +0200

    Add codecompanion

---
+++ lua/custom/plugins/codecompanion.lua
@@ -0,0 +1,36 @@
+return {
+  'olimorris/codecompanion.nvim',
+  opts = {},
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+  },
+  config = function()
+    require('codecompanion').setup {
+      strategies = {
+        chat = {
+          adapter = 'gemini_cli',
+        },
+        inline = {
+          adapter = 'gemini_cli',
+        },
+        cmd = {
+          adapter = 'gemini_cli',
+        },
+      },
+      adapters = {
+        acp = {
+          gemini_cli = function()
+            return require('codecompanion.adapters').extend('gemini_cli', {
+              commands = {
+                default = {
+                  '/opt/homebrew/bin/gemini',
+                  '--experimental-acp',
+                },
+              },
+            })
+          end,
+        },
+      },
+    }
+  end,
+}
