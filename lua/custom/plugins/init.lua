395dd8943ea3a3fec75a1139e4026414cc135250
Athavan Kanapuli <athavankanapuli@gmail.com>
2025-10-03 15:33:18 +0200

    add keymap for no neck pain

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,5 +3,12 @@
 --
 -- See the kickstart.nvim README for more information
 return {
-  { 'shortcuts/no-neck-pain.nvim' },
+  {
+    'shortcuts/no-neck-pain.nvim',
+    config = function()
+      require('no-neck-pain').setup {
+        vim.keymap.set('n', '<leader>np', '<cmd>NoNeckPain<CR>', { desc = '[N]o [N]eck [P]ain' }),
+      }
+    end,
+  },
 }
