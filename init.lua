be6065d2157df5f6d14d4e583b83e93ab3edbc9f
Athavan Kanapuli <athavankanapuli@gmail.com>
2025-10-22 12:28:36 +0200

    add refaoctoring of null-ls

--- init.lua
+++ init.lua
@@ -843,32 +843,31 @@ require('lazy').setup({
         desc = '[F]ormat buffer',
       },
     },
+    config = function()
+      require('conform').setup {
+        format_on_save = {
+          timeout_ms = 500,
+          lsp_format = 'fallback',
+        },
+        formatters_by_ft = {
+          lua = { 'stylua' },
+          go = { 'goimports-reviser', 'gofumpt', 'golines' },
+          -- Conform can also run multiple formatters sequentially
+          python = { 'isort', 'black' },
+          sh = { 'shfmt' },
+          --
+          -- You can use 'stop_after_first' to run the first available formatter from the list
+          -- javascript = { "prettierd", "prettier", stop_after_first = true },
+        },
+        -- formatters = {
+        --   ['goimports-reviser'] = {
+        --     prepend_args = { '-rm-unused' },
+        --   },
+        -- },
+      }
+    end,
     opts = {
       notify_on_error = true,
-      format_on_save = function(bufnr)
-        -- Disable "format_on_save lsp_fallback" for languages that don't
-        -- have a well standardized coding style. You can add additional
-        -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
-        if disable_filetypes[vim.bo[bufnr].filetype] then
-          return nil
-        else
-          return {
-            timeout_ms = 500,
-            lsp_format = 'fallback',
-          }
-        end
-      end,
-      formatters_by_ft = {
-        lua = { 'stylua' },
-        go = { 'goimports-reviser', 'gofumpt' },
-        -- Conform can also run multiple formatters sequentially
-        python = { 'isort', 'black' },
-        sh = { 'shfmt' },
-        --
-        -- You can use 'stop_after_first' to run the first available formatter from the list
-        -- javascript = { "prettierd", "prettier", stop_after_first = true },
-      },
     },
   },
 
--- lua/custom/plugins/none-ls.lua
+++ lua/custom/plugins/none-ls.lua
@@ -1,9 +1,22 @@
 return {
   'nvimtools/none-ls.nvim',
+  enabled = true,
   dependencies = {
     {
       'mason-org/mason.nvim',
       opts = { ensure_installed = { 'gomodifytags', 'impl' } },
+    },
+    {
+      'ThePrimeagen/refactoring.nvim',
+      dependencies = {
+        'nvim-lua/plenary.nvim',
+        'nvim-treesitter/nvim-treesitter',
+      },
+      lazy = false,
+      opts = {},
+      config = function()
+        require('refactoring').setup {}
+      end,
     },
   },
   opts = function(_, opts)
@@ -11,8 +24,13 @@ return {
     opts.sources = vim.list_extend(opts.sources or {}, {
       nls.builtins.code_actions.gomodifytags,
       nls.builtins.code_actions.impl,
-      nls.builtins.formatting.goimports,
-      nls.builtins.formatting.gofumpt,
+      -- nls.builtins.formatting.goimports_reviser.with {
+      --   -- extra_args = { '-rm-unused' },
+      -- },
+      nls.builtins.code_actions.gitsigns,
+      nls.builtins.code_actions.refactoring,
+      nls.builtins.completion.tags,
+      nls.builtins.diagnostics.mypy,
     })
   end,
 }
